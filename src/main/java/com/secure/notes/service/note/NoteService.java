bed35690be64a00c3b8b9c6d652cf3e28a0eb7fa
mabdelmageedali <ma@bdelmageed2004@gmail.com>
2026-07-14 17:43:32 +0300

    transfer to JdbcUserDetailsManager

--- src/main/java/com/secure/notes/service/note/NoteService.java
+++ src/main/java/com/secure/notes/service/note/NoteService.java
@@ -1,11 +1,8 @@
 package com.secure.notes.service.note;
 
 import com.secure.notes.model.Note;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
 public interface NoteService {
     Note createNoteForUser (String username, String content);
     Note updateNoteForUser (Long id, String content, String username);
